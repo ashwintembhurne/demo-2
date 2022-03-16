f790eb4e363d75772b566031c43c631d41eb1ec1
ashwintembhurne <101572470+ashwintembhurne@users.noreply.github.com>
2022-03-16 14:48:29 +0530

    java swap

---
+++ swap.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class swap {
+
+	public static void main(String[] args) {
+	int a=10;
+	int b=30;
+	System.out.println("before swapping "+a+"  "+b);
+	System.out.println("____________________________________________");
+	int temp=a;
+	
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println(" \n after swapping "+a+"  "+b);
+
+	}
+
+}
