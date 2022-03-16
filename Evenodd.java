786654bf184177238e1dfdba9b5ffb4bfd27284b
ashwintembhurne <101572470+ashwintembhurne@users.noreply.github.com>
2022-03-16 14:44:56 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Evenodd {
+
+	public static void main(String[] args) 
+	{
+		int a=59;
+		if (a%2==0)
+			System.out.println("number is even");
+		else
+			System.out.println("number is odd");
+		
+	}
+
+}
